fda6bbbd904bf426aaad65bbee23c8f308d6d451
fanliwen <fanliwen@wandoujia.com>
2016-08-22 13:07:19 +0800

    fix viper randselect read mat

--- src/main/java/com/github/levyfan/reid/eval/Viper.java
+++ src/main/java/com/github/levyfan/reid/eval/Viper.java
@@ -4,6 +4,7 @@ import com.github.levyfan.reid.pca.BlasPca;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.jmatio.io.MatFileReader;
+import com.jmatio.io.MatFileType;
 import com.jmatio.types.MLDouble;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
@@ -12,8 +13,8 @@ import org.apache.commons.math3.util.Pair;
 import org.jblas.DoubleMatrix;
 import org.jblas.ranges.IntervalRange;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -25,10 +26,11 @@ public class Viper {
     private RealMatrix select;
 
     public Viper() throws URISyntaxException, IOException {
-        File file = new File(this.getClass().getResource("/randselect10.mat").toURI());
-        MatFileReader reader = new MatFileReader(file);
-        MLDouble mlDouble = (MLDouble) reader.getMLArray("selectsample");
-        this.select = MatrixUtils.createRealMatrix(mlDouble.getArray());
+        try (InputStream stream = this.getClass().getResourceAsStream("/randselect10.mat")) {
+            MatFileReader reader = new MatFileReader(stream, MatFileType.Regular);
+            MLDouble mlDouble = (MLDouble) reader.getMLArray("selectsample");
+            this.select = MatrixUtils.createRealMatrix(mlDouble.getArray());
+        }
     }
 
     public double[] eval(RealMatrix score) {
